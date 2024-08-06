5afe6d1f66d1a9c0b6b0b1c3a9dee3b7e291e9dd
unknown <eldergsilva@gmail.com>
2024-08-06 09:00:18 -0300

    alterado package.json raiz

--- app/js/usuario/listar copy.js
+++ app/js/usuario/listar copy.js
@@ -1,13 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Função para formatar a data no formato dd/mm/yyyy
-    function formatDate(dateString) {
-        const date = new Date(dateString);
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const year = date.getFullYear();
-        return `${day}/${month}/${year}`;
-    }
-
     // Função para criar cartões de usuário
     function createUserCards(users) {
         const userList = document.getElementById('userList');
@@ -20,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 <h3>Nome: ${user.nome}</h3>
                 <p>Email: ${user.email}</p>
                 <p>CPF: ${user.cpf}</p>
-                <p>Data Nasc: ${formatDate(user.dataNascimento)}</p>
+                <p>Data de Nascimento: ${user.dataNascimento}</p>
             `;
             userList.appendChild(card);
         });

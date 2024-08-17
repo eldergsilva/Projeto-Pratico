627b8d9e48c613047905ac2df1575db600a3e72a
unknown <eldergsilva@gmail.com>
2024-08-17 12:04:41 -0300

    adicionado o tela de administração

--- api/src/controllers/carrinhoController.js
+++ api/src/controllers/carrinhoController.js
@@ -2,7 +2,6 @@ const CarrinhoService = require('../services/carrinhoService');
 const carrinhoService = new CarrinhoService();
 
 class CarrinhoController {
-    // Cria um novo carrinho para o usuário
     static async criarCarrinho(req, res) {
         const { usuario_id } = req.body;
         try {
@@ -13,10 +12,9 @@ class CarrinhoController {
         }
     }
 
-    // Adiciona um item ao carrinho
     static async adicionarItem(req, res) {
-        const { carrinhoId, produtoId } = req.params; // Obtém os IDs da URL
-        const { quantidade } = req.body; // Obtém a quantidade do corpo da requisição
+        const { carrinhoId, produtoId } = req.params;
+        const { quantidade } = req.body;
         try {
             const carrinho = await carrinhoService.adicionarItem(carrinhoId, produtoId, quantidade);
             res.status(200).json(carrinho);
@@ -25,9 +23,8 @@ class CarrinhoController {
         }
     }
 
-    // Remove um item do carrinho
     static async removerItem(req, res) {
-        const { carrinhoId, produtoId } = req.params; // Obtém os IDs da URL
+        const { carrinhoId, produtoId } = req.params;
         try {
             const carrinho = await carrinhoService.removerItem(carrinhoId, produtoId);
             res.status(200).json(carrinho);
@@ -36,9 +33,8 @@ class CarrinhoController {
         }
     }
 
-    // Busca o carrinho de um usuário
     static async buscarCarrinhoPorUsuario(req, res) {
-        const { usuario_id } = req.params; // Obtém o ID do usuário da URL
+        const { usuario_id } = req.params;
         try {
             const carrinho = await carrinhoService.buscarCarrinhoPorUsuario(usuario_id);
             res.status(200).json(carrinho);
@@ -47,12 +43,15 @@ class CarrinhoController {
         }
     }
 
-    // Busca um carrinho pelo ID
     static async buscarCarrinhoPorId(req, res) {
-        const { carrinhoId } = req.params; // Obtém o ID do carrinho da URL
+        const { carrinhoId } = req.params;
         try {
             const carrinho = await carrinhoService.buscarCarrinhoPorId(carrinhoId);
-            res.status(200).json(carrinho);
+            if (carrinho) {
+                res.status(200).json(carrinho);
+            } else {
+                res.status(404).send({ message: 'Carrinho não encontrado' });
+            }
         } catch (error) {
             res.status(400).send({ message: error.message });
         }
--- api/src/routes/carrinhoRoute.js
+++ api/src/routes/carrinhoRoute.js
@@ -1,12 +1,18 @@
 const { Router } = require('express');
+
 const CarrinhoController = require('../controllers/carrinhoController');
+
 const autenticado = require('../middleware/autenticado');
 
 const router = Router();
-
 router.post('/carrinho', autenticado, CarrinhoController.criarCarrinho);
+
 router.get('/carrinho/:usuario_id', autenticado, CarrinhoController.buscarCarrinhoPorUsuario);
+
 router.put('/carrinho/:carrinhoId/produto/:produtoId', autenticado, CarrinhoController.adicionarItem);
 
 router.delete('/carrinho/:carrinhoId/produto/:produtoId', autenticado, CarrinhoController.removerItem);
+
+router.get('/carrinho/id/:carrinhoId', autenticado, CarrinhoController.buscarCarrinhoPorId);
+
 module.exports = router;
